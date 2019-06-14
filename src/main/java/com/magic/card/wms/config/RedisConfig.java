4bfaf01a27be591a925f885c8a3919a8d776e37b
pengjie <pengjie@LAPTOP-SLNVL1O6>
2019-06-14 10:05:58 +0800

    redis提交
    Signed-off-by: pengjie <pengjie@LAPTOP-SLNVL1O6>

--- src/main/java/com/magic/card/wms/config/RedisConfig.java
+++ src/main/java/com/magic/card/wms/config/RedisConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 /**
@@ -22,7 +23,9 @@ public class RedisConfig {
 		template.setConnectionFactory(redisConnectionFactory);
 		template.afterPropertiesSet();
 		template.setKeySerializer(new StringRedisSerializer());
+		template.setValueSerializer(new Jackson2JsonRedisSerializer(Object.class));
 		return template;
 	}
+
 	
 }
